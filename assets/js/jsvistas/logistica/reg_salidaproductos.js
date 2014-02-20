bb93bb494b3730d489edc31e2e191207a2b5d261
jalcantara <jomialpe123>
2014-02-20 17:45:01 -0500

    changes

--- assets/js/jsvistas/logistica/reg_salidaproductos.js
+++ assets/js/jsvistas/logistica/reg_salidaproductos.js
@@ -1,6 +1,10 @@
 	$(document).ready(function(){
 
+//<<<<<<< Updated upstream
 		$('#btn-trabajador').click(function(e){
+//=======
+		$('#btn-buscar-trabajador').click(function(e){
+//>>>>>>> Stashed changes
 			e.preventDefault();
 			$('#modalBuscarTrabajador').modal('show');
 		});
